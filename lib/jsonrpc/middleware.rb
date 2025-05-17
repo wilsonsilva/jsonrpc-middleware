745b5a498dc738c52605e1f54c1f9db4a433e820
Wilson Silva <wilson.dsigns@gmail.com>
2025-05-17 14:10:13 +0100

    Initial commit

---
+++ lib/jsonrpc/middleware.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "middleware/version"
+
+module Jsonrpc
+  module Middleware
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
